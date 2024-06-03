7ca54ebe17a9d1f3cc3d27476a2d59e8e9b55846
DanielCoelho00 <daniel.coelholeite@hotmail.com>
2024-06-03 19:50:14 -0300

    atualização

--- efeito.js
+++ efeito.js
@@ -73,33 +73,33 @@ revelar.reveal('.efeito-img',
         duration: 2000,
         distance: '400px',
         origin: 'left',
-       
+
     })
 
-    revelar.reveal('.efeito-txt',
+revelar.reveal('.efeito-txt',
     {
         duration: 2000,
         distance: '400px',
         origin: 'right',
-       
+
     })
 
-    //responsivo
-    let btnmenu = document.getElementById('btn-menu')
-    let menu = document.getElementById('menu-mobile')
-    let overlay = document.getElementById('overlay-menu')
+//responsivo
+let btnmenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
-    btnmenu.addEventListener('click',()=>{
-    menu.classList.add('abrir-menu')    
-    })
+btnmenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
 
-    menu.addEventListener('click',()=>{
-        menu.classList.remove('abrir-menu')    
-        })
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
 
-        overlay.addEventListener('click',()=>{
-            menu.classList.remove('abrir-menu')    
-            })
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
 
 
 
